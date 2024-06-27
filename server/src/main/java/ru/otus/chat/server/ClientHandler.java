f7fd0b94a30c661f505ecce40d667bf4ddd4eb71
BMV <venerasmith@mail.ru>
2024-06-27 23:06:43 +0300

    feat: hw-15 init project

---
+++ server/src/main/java/ru/otus/chat/server/ClientHandler.java
@@ -0,0 +1,92 @@
+package ru.otus.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private String username;
+
+    private static int usersCount = 0;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public ClientHandler(Server server, Socket socket) throws IOException {
+        this.server = server;
+        this.socket = socket;
+        this.in = new DataInputStream(socket.getInputStream());
+        this.out = new DataOutputStream(socket.getOutputStream());
+        usersCount++;
+        this.username = "user" + usersCount;
+        new Thread(() -> {
+            try {
+                System.out.println("Подключился новый клиент");
+                while (true) {
+                    String message = in.readUTF();
+                    if (message.startsWith("/")) {
+                        if (message.equals("/exit")) {
+                            sendMessage("/exitok");
+                            break;
+                        }
+                        if (message.startsWith("/w ")) {
+                            var args = message.split("\\s+");
+                            var to = args[1];
+                            var text = Arrays.stream(args).skip(2).collect(Collectors.joining(" "));
+                            server.sendMessageTo(username, to, text);
+                            continue;
+                        }
+
+                    }
+                    server.broadcastMessage("От " + username + ": " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                disconnect();
+            }
+        }).start();
+    }
+
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        server.unsubscribe(this);
+        try {
+            if (in != null) {
+                in.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ server/src/main/java/ru/otus/chat/server/Server.java
@@ -0,0 +1,56 @@
+package ru.otus.chat.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    private int port;
+    private List<ClientHandler> clients;
+
+    public Server(int port) {
+        this.port = port;
+        this.clients = new ArrayList<>();
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен на порту: " + port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                subscribe(new ClientHandler(this, socket));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void subscribe(ClientHandler clientHandler) {
+        broadcastMessage("В чат зашел: " + clientHandler.getUsername());
+        clients.add(clientHandler);
+    }
+
+    public synchronized void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+        broadcastMessage("Из чата вышел: " + clientHandler.getUsername());
+    }
+
+    public synchronized void broadcastMessage(String message) {
+        for (ClientHandler c : clients) {
+            c.sendMessage(message);
+        }
+    }
+
+    public synchronized void sendMessageTo(String from, String to, String message) {
+        var client = clients.stream().filter(c -> c.getUsername().equalsIgnoreCase(to)).findFirst();
+        if (client.isPresent()) {
+            client.get().sendMessage("От " + to + ": " + message);
+            client = clients.stream().filter(c -> c.getUsername().equalsIgnoreCase(from)).findFirst();
+            client.ifPresent(clientHandler -> clientHandler.sendMessage("Для " + to + ": " + message));
+            return;
+        }
+        client = clients.stream().filter(c -> c.getUsername().equalsIgnoreCase(from)).findFirst();
+        client.ifPresent(clientHandler -> clientHandler.sendMessage("Сообщение не дошло до получателя " + to));
+    }
+}
