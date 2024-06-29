3ab5fa50a016df33d271efcb41c645a926b9a059
BMV <venerasmith@mail.ru>
2024-06-29 19:04:24 +0300

    feat: lesson 23 - registration & authentication added

--- server/src/main/java/ru/otus/chat/server/Server.java
+++ server/src/main/java/ru/otus/chat/server/Server.java
@@ -8,18 +8,25 @@ import java.util.List;
 public class Server {
     private int port;
     private List<ClientHandler> clients;
+    private AuthenticationProvider authenticationProvider;
+
+    public AuthenticationProvider getAuthenticationProvider() {
+        return authenticationProvider;
+    }
 
     public Server(int port) {
         this.port = port;
         this.clients = new ArrayList<>();
+        this.authenticationProvider = new InMemoryAuthenticationProvider(this);
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
+            authenticationProvider.initialize();
             while (true) {
                 Socket socket = serverSocket.accept();
-                subscribe(new ClientHandler(this, socket));
+                new ClientHandler(this, socket);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,5 +58,14 @@ public class Server {
             return;
         }
         fromClient.sendMessage("Сообщение не дошло до получателя " + to);
+    }
+
+    public boolean isUsernameBusy(String username) {
+        for (ClientHandler c : clients) {
+            if (c.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
